c2342d53932eb5e5271dc84a90951b7d7ffe2e7b
袁秉瑞 <bingrui.yuan@roadshare.com>
2017-11-27 17:47:18 +0800

    add carousel component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
     <topMenu :menu-data="menuData"/>
+    <carousel :carousel-data="carouselData"/>
   </div>
 </template>
 
 <script>
 import topMenu from './components/topMenu'
+import carousel from './components/carousel'
 export default {
   name: 'app',
   components: {
-    topMenu
+    topMenu,
+    carousel
   },
   data () {
     return {
@@ -18,7 +21,8 @@ export default {
         slideSpeed: '',
         logoImg: '../../static/logo.png',
         menuArr: [{name: 'Core Docs', href: '#'}, {name: 'Forum', href: '#'}, {name: 'Community', href: '#'}, {name: 'Twitter', href: '#'}, {name: 'Vue Router', href: '#'}, {name: 'Vuex', href: '#'}, {name: 'Typescript', href: '#'}]
-      }
+      },
+      carouselData: {}
     }
   }
 }
---
+++ src/components/carousel.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="carouselBox">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'carousel',
+  props: ['carouselData'],
+  data () {
+    return {}
+  },
+  mounted: function () {},
+  methods: {}
+}
+</script>
+
+<style scoped>
+#carouselBox{
+
+}
+</style>
